98a3f9f00a38d5fb4b59e2eec8ccd84a3c5b3594
Max Chernogor <mche@users.sourceforge.net>
2003-02-19 16:27:33 +0000

    fix detection end of section

--- src/scanmsg.c
+++ src/scanmsg.c
@@ -63,7 +63,6 @@ void _addPart(char *text, int section, int amount, char* name, char* ID, int typ
         partlen = 3*(endstr-end-1)/4;
         if(DECODE_BYTE (end[0]) != partlen)
         {
-            w_log('1',"%d %d %d %c", DECODE_BYTE (end[0]) , partlen, section, *(endstr-1));
             if(!(((*(endstr-1) == '`') || (*(endstr+1) == '`'))&& (DECODE_BYTE(end[0]) == partlen-1)))
             {
                 break;
