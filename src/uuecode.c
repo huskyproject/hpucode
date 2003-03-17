16c05a15ddb730189b47656a29e789a380ba8c22
Stas Degteff <stas_degteff@users.sourceforge.net>
2003-03-17 13:08:47 +0000

    Hide getctab() and arrays intab, outtab: new function getctabs()

--- src/uuecode.c
+++ src/uuecode.c
@@ -202,8 +202,7 @@ int main(int argc, char **argv) {
         }
         /*  load recoding tables */
         initCharsets();
-        if (config->intab != NULL) getctab(intab, (unsigned char*) config->intab);
-        if (config->outtab != NULL) getctab(outtab, (unsigned char*) config->outtab);
+        getctabs(config->intab,config->outtab);
 
         if (config->logFileDir) {
             nfree(buff);
